1b525cd3dddadf64846e20d9caf5303c84faeb34
keitei <keitei@live.com>
2015-06-15 21:26:32 +0800

    2015060152125

--- projects/app/www/js/app.js
+++ projects/app/www/js/app.js
@@ -3,34 +3,24 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-<<<<<<< Updated upstream
 angular.module('cheping', ['ionic', 'cheping.controllers', 'cheping.services'])
-=======
-angular.module('myCar', ['ionic', 'ngCordova'])
 
-.run(function($cordovaSplashscreen) {
-    setTimeout(function() {
-        $cordovaSplashscreen.hide()
-    }, 5000)
-})
->>>>>>> Stashed changes
-
-  .run(function($ionicPlatform) {
+.run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        cordova.plugins.Keyboard.disableScroll(true);
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleLightContent();
-      }
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleLightContent();
+        }
     });
-  })
+})
 
-  .config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
 
     // Ionic uses AngularUI Router which uses the concept of states
     // Learn more here: https://github.com/angular-ui/ui-router
@@ -38,66 +28,66 @@ angular.module('myCar', ['ionic', 'ngCordova'])
     // Each state's controller can be found in controllers.js
     $stateProvider
 
-      // setup an abstract state for the tabs directive
-      .state('tabs', {
+    // setup an abstract state for the tabs directive
+        .state('tabs', {
         url: "/tabs",
         abstract: true,
         templateUrl: "templates/tabs.html"
-      })
+    })
 
-      // Each tab has its own nav history stack:
+    // Each tab has its own nav history stack:
 
-      .state('tabs.todos', {
-        url: '/todos',
-        views: {
-          'tabs.todos': {
-            templateUrl: 'templates/tabs.todos.html',
-            controller: 'TodosCtrl'
-          }
-        }
-      })
-      .state('tabs.todoDetail', {
-        url: '/todos/{orderNo}',
-        views: {
-          'tabs.todos': {
-            templateUrl: 'templates/todoDetail.html',
-            controller: 'TodoDetailCtrl'
-          }
-        }
-      })
+    .state('tabs.todos', {
+            url: '/todos',
+            views: {
+                'tabs.todos': {
+                    templateUrl: 'templates/tabs.todos.html',
+                    controller: 'TodosCtrl'
+                }
+            }
+        })
+        .state('tabs.todoDetail', {
+            url: '/todos/{orderNo}',
+            views: {
+                'tabs.todos': {
+                    templateUrl: 'templates/todoDetail.html',
+                    controller: 'TodoDetailCtrl'
+                }
+            }
+        })
 
-      .state('tabs.orders', {
-        url: '/orders',
-        views: {
-          'tabs.orders': {
-            templateUrl: 'templates/tabs.orders.html',
-            controller: 'OrdersCtrl'
-          }
-        }
-      })
-      .state('tabs.orderDetail', {
-        url: '/orders/{orderNo}',
-        views: {
-          'tabs.orders': {
-            templateUrl: 'templates/orderDetail.html',
-            controller: 'OrderDetailCtrl'
-          }
-        }
-      })
-      .state('tabs.newOrder', {
-        url: '/newOrder',
-        views: {
-          'tabs.newOrder': {
-            templateUrl: 'templates/tabs.newOrder.html',
-            controller: 'NewOrderCtrl'
-          }
-        }
-      });
+    .state('tabs.orders', {
+            url: '/orders',
+            views: {
+                'tabs.orders': {
+                    templateUrl: 'templates/tabs.orders.html',
+                    controller: 'OrdersCtrl'
+                }
+            }
+        })
+        .state('tabs.orderDetail', {
+            url: '/orders/{orderNo}',
+            views: {
+                'tabs.orders': {
+                    templateUrl: 'templates/orderDetail.html',
+                    controller: 'OrderDetailCtrl'
+                }
+            }
+        })
+        .state('tabs.newOrder', {
+            url: '/newOrder',
+            views: {
+                'tabs.newOrder': {
+                    templateUrl: 'templates/tabs.newOrder.html',
+                    controller: 'NewOrderCtrl'
+                }
+            }
+        });
 
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/tabs/todos');
 
-  });
+});
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
--- src/www/app/index/order/ordes.js
+++ src/www/app/index/order/ordes.js
@@ -11,9 +11,62 @@ angular.module('cheping.index.orders', [
                     }
                 }
             })
+          .state('cheping.index.orderList', {
+                url: '/orders/list',
+                views: {
+                    'index.orders': {
+                      controller: 'OrderListCtrl as orderListCtrl',
+                      templateUrl: 'app/index/order/order-list.tpl.html'
+                    }
+                }
+          })
+          .state('cheping.index.orderListDetail', {
+                url: '/orders/list/detail',
+                views: {
+                    'index.orders': {
+                      controller: 'OrderListDetailCtrl as orderListDetailCtrl',
+                      templateUrl:'app/index/order/order-detail.tpl.html'
+                    }
+                }
+          });
+          //.state('cheping.index.orderList-detail', {
+          //  url: '/orders/list/detail',
+          //  views: {
+          //    'index.orders': {
+          //      controller: 'OrderDetailCtrl as orderDetailCtrl',
+          //      templateUrl: 'app/index/order/order-detail.tpl.html'
+          //    }
+          //  }
+          //})
     })
     .controller('OrdersCtrl', function(ChepingOrderService) {
         var ordersCtrl = this;
-        ordersCtrl.orders = ChepingOrderService.getOrders();
 
-    });
+    })
+    .controller('OrderListCtrl', function($state, $stateParams, ChepingOrderService) {
+     var orderListCtrl = this;
+         orderListCtrl.orderList = ChepingOrderService.getOrders();
+         orderListCtrl.goBack = function() {
+           $state.go('cheping.index.orders');
+         }
+  })
+     .controller('OrderListDetailCtrl', function($state, $stateParams, ChepingOrderService) {
+     var orderListDetailCtrl = this;
+         orderListDetailCtrl.order = ChepingOrderService.getOrder($stateParams.orderNo);
+         orderListDetailCtrl.goBack = function() {
+           $state.go('cheping.index.orderList');
+         }
+  });
+
+//
+//.controller('CreateDetailCtrl', function($state, $stateParams, ChepingOrderService) {
+//  var createDetailCtrl = this;
+//  var newOrder = ChepingOrderService.getNewOrder();
+//
+//  newOrder.carType = $stateParams.carType;
+//
+//  createDetailCtrl.newOrder = newOrder;
+//
+//  createDetailCtrl.goBack = function(){
+//    $state.go('cheping.index.create');
+//  };
