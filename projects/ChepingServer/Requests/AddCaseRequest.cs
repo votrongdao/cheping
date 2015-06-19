46f5c73410b8a117cc694c795dd095abf83a80e3
sunbeams001 <sunbeams001@gmail.com>
2015-06-19 19:42:01 +0800

    201506191941

--- projects/ChepingServer/Requests/AddCaseRequest.cs
+++ projects/ChepingServer/Requests/AddCaseRequest.cs
@@ -46,61 +46,73 @@ namespace ChepingServer.Requests
         /// <summary>
         ///     表显里程
         /// </summary>
+        [Required, Range(0, int.MaxValue), JsonProperty("displayMileage")]
         public int DisplayMileage { get; set; }
 
         /// <summary>
         ///     心理价格
         /// </summary>
+        [Required, Range(0, int.MaxValue), JsonProperty("expectedPrice")]
         public int ExpectedPrice { get; set; }
 
         /// <summary>
         ///     出厂时间
         /// </summary>
+        [Required, JsonProperty("factoryTime")]
         public DateTime FactoryTime { get; set; }
 
         /// <summary>
         ///     内部颜色
         /// </summary>
+        [Required, Range(0, int.MaxValue), JsonProperty("innerColor")]
         public int InnerColor { get; set; }
 
         /// <summary>
-        ///     外部颜色
+        ///     牌照所在地
         /// </summary>
+        [Required, Range(0, int.MaxValue), JsonProperty("licenseLocation")]
         public string LicenseLocation { get; set; }
 
         /// <summary>
         ///     上牌时间
         /// </summary>
+        [Required, JsonProperty("licenseTime")]
         public DateTime LicenseTime { get; set; }
 
         /// <summary>
         ///     车型Id
         /// </summary>
+        [Required, Range(0, int.MaxValue), JsonProperty("modelId")]
         public int ModelId { get; set; }
 
         /// <summary>
         ///     车型名称
         /// </summary>
+        [Required, StringLength(200, MinimumLength = 1), JsonProperty("modelName")]
         public string ModelName { get; set; }
 
         /// <summary>
         ///     改装内容
         /// </summary>
+        [Required, StringLength(int.MaxValue,MinimumLength=1), JsonProperty("modifiedContent")]
         public string ModifiedContent { get; set; }
 
         /// <summary>
         ///     外部颜色
         /// </summary>
+        [Required, Range(0, int.MaxValue), JsonProperty("outerColor")]
         public int OuterColor { get; set; }
 
         /// <summary>
         ///     车系名称
         /// </summary>
+        [Required, StringLength(200, MinimumLength = 1), JsonProperty("seriesName")]
         public string SeriesName { get; set; }
 
         /// <summary>
         ///     车辆所在地
         /// </summary>
+        [Required, StringLength(200, MinimumLength = 1), JsonProperty("vehicleLocation")]
         public string VehicleLocation { get; set; }
     }
 }
