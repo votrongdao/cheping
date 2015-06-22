1d98b25fed9412b1b8260c9d3dc2bd5290dee25d
keitei <keitei@live.com>
2015-06-22 20:46:32 +0800

    201506222045

--- projects/app/www/js/app.js
+++ projects/app/www/js/app.js
@@ -3,7 +3,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('cheping', ['ionic', 'cheping.controllers', 'cheping.services'])
+angular.module('cheping', ['ionic', 'cheping.controllers', 'cheping.services', 'ngCordova'])
 
 .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
