e35806eba0f59b839a2dd652e61580d29e8d1651
keitei <keitei@live.com>
2015-06-24 00:20:42 +0800

    201506240020

--- projects/app/www/js/app.js
+++ projects/app/www/js/app.js
@@ -3,7 +3,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('cheping', ['ionic', 'cheping.controllers', 'cheping.services', 'ngCordova'])
+angular.module('cheping', ['ionic', 'cheping.controllers', 'cheping.services'])
 
 .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
@@ -18,6 +18,11 @@ angular.module('cheping', ['ionic', 'cheping.controllers', 'cheping.services', '
             StatusBar.styleLightContent();
         }
     });
+})
+.run(function($cordovaSplashscreen) {
+  setTimeout(function() {
+    $cordovaSplashscreen.hide()
+  }, 5000)
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
